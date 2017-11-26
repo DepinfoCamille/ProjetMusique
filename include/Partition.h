42829abf938bfbd060888fe961568a9dc87e4fc4
DepinfoCamille <camilletruong@orange.fr>
2017-11-26 17:07:09 +0100

    Création de la classe partition et de la lecture clavier

--- include/Partition.h
+++ include/Partition.h
@@ -1,16 +1,16 @@
-#ifndef PARTITION_H
-#define PARTITION_H
-
-
+#pragma once
+#include <vector>
+using std::vector ;
 class Partition
 {
     public:
         Partition();
+        void ajoutNote(char c,int t) ;
         virtual ~Partition();
 
     protected:
 
     private:
+        vector<char> listeNotes ; // stocke toutes les notes tapées au clavier
+        vector<int> listeTemps ; // stocke le temps que cette note est jouée
 };
-
-#endif // PARTITION_H
