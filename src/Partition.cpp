079bbd16b0ee8508ad115a420790cb1fe15cb310
DepinfoCamille <camilletruong@orange.fr>
2017-12-05 16:11:11 +0100

    Conversion d'un caractère en note grâce au dictionnaire

--- src/Partition.cpp
+++ src/Partition.cpp
@@ -5,36 +5,50 @@
 
 
 Partition::Partition(){
-<<<<<<< HEAD
-    this->listeNotes = new std::vector<char*>() ;
-    this->listeTemps = new std::vector<int>() ;
-=======
-//    listeNotes = new vector<char>() ;
+//    this->listeNotes = new std::vector<char*>() ;
+ //   this->listeTemps = new std::vector<int>() ;
     this ->dicco_notes = {
    {'q',std::make_tuple("DO",3)},{'z',std::make_tuple("DO#",3)}, {'s',std::make_tuple("RE",3)}, {'e',std::make_tuple("RE#",3)},
        {'d',std::make_tuple("MI",3)}, {'f',std::make_tuple("FA",3)}, {'t',std::make_tuple("FA#",3)}, {'g',std::make_tuple("SOL",3)},
         {'y',std::make_tuple("SOL#",3)}, {'h',std::make_tuple("LA",3)},{'u',std::make_tuple("LA#",3)}, {'j',std::make_tuple("SI",3)},
         {'k',std::make_tuple("DO",2)}, {'o',std::make_tuple("DO#",2)}, {'l',std::make_tuple("RE",2)}};
->>>>>>> 0c121cb7f7a5806793ed74106cdc296272e5cfe6
 }
 
 /* cette fonction ajoute une note qui dure t ms à la partition */
 void Partition::ajoutNote(char c, int t){
-        this->listeClavier.push_back(c) ;
+        std::string note ;
+        int octave = -1 ;
+
+        // on associe le caractère entré à une note donnée
+        std::unordered_map<char,std::tuple<std::string,int>>::iterator it;
+        // Find if an element with key exists or not.
+        // find() returns an iterator
+        it = this->dicco_notes.find(c);
+        // Check if iterator points to end of map
+        if (it != this->dicco_notes.end()){
+
+            note = std::get<0>(it->second) ;
+            octave = std::get<1>(it->second) ;
+        }
+        else{ // erreur de frappe
+            note = "SILENCE" ;
+        }
+        // on ajoute la note à la partition
+        this->listeNotes.push_back(note) ;
+        this->listeOctave.push_back(octave) ;
         this->listeTemps.push_back(t) ;
+
 }
 
 /* cette fonction lit ce que l'utilisateur tape au clavier et le
  * sauvegarde dans listeNotes et listeTemps */
-int Partition::lectureClavier()
-{
+int Partition::lectureClavier(){
     clock_t start = clock(), diff;
     char caractere = getch() ;
     diff = clock() - start;
 
     if (caractere!='\n'){
-        this->ajoutNote(caractere,diff) ;
-        std::cout<<caractere<<std::endl ;
+        this->Partition::ajoutNote(caractere,diff) ;
         return 0 ;
     }
     else{
