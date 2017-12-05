f799d35902bc7fb2aa19b68e6a074f0a83359a7a
DepinfoCamille <camilletruong@orange.fr>
2017-12-05 14:53:45 +0100

    Transformationde ce que l'utilisateur tape au clavier en partition

--- include/Partition.h
+++ include/Partition.h
@@ -1,16 +1,25 @@
 #pragma once
 #include <vector>
 using std::vector ;
+
+
+
+
 class Partition
 {
     public:
         Partition();
         void ajoutNote(char c,int t) ;
         virtual ~Partition();
+        int lectureClavier() ;  // quand lectureClavier renvoie un 1, cela signifie que l'utilisateur a tapé '\n'
+                                // c'est la fin de la partition
+
 
     protected:
 
     private:
-        vector<char> listeNotes ; // stocke toutes les notes tapées au clavier
+        vector<char> listeClavier ; // stocke toutes les notes tapées au clavier sous forme de caractères 'q', 's'...
+        vector<char*> listeNotes ; // stocke toutes les notes tapées au clavier sous forme de notes "DO", "RE"...
         vector<int> listeTemps ; // stocke le temps que cette note est jouée
+
 };
