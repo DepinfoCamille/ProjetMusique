65e2ec8c9128aa5fc674c1897020771482370721
DepinfoCamille <camilletruong@orange.fr>
2017-12-05 14:00:02 +0100

    Ajout du main

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "Partition.h"
+#include "Clavier.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+char saisie[2048];
+
+void lectureClavier()
+{
+    if(!fgets(saisie,sizeof(saisie)-1,stdin))
+    {
+        std::cout<<std::endl ;
+        exit(0);
+    }
+
+
+}
+
+int main(){
+
+    while(1){
+        lectureClavier() ;
+    }
+
+}
