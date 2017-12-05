cb3f1b7f98ff4a0b0aa796dba0ff3c16af2db496
MariaSalvador <maria.salvador-masip2@etu.univ-lorraine.fr>
2017-12-05 15:07:20 +0100

    modification du test

--- src/testdiccionnaire.cpp
+++ src/testdiccionnaire.cpp
@@ -6,17 +6,16 @@
 
 int main() {
 	// Initialize an unordered_map through initializer_list
-
+    Partition* test = new Partition();
 	// Declare an iterator to unordered_map
 	std::unordered_map<char,std::tuple<std::string,int>>::iterator it;
 
 	// Find if an element with key "First" exists or not.
 	// find() returns an iterator
-
-	it = dicco_notes.find('q');
+	it = test->dicco_notes.find('j');
 
 	// Check if iterator points to end of map
-	if (it != dicco_notes.end())
+	if (it != test->dicco_notes.end())
 	{
 		std::cout << "Element Found - ";
 		std::cout << it->first << "::"<< std::get<0>(it->second) << " "<<std::get<1>(it->second) << std::endl;
